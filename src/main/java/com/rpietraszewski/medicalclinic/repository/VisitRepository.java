04e4f01dc78644bd1a0bf7a227fb0a602d1e9e50
Robert Pietraszewski <80597@student.pb.edu.pl>
2024-02-27 13:36:28 +0100

    + MED-97

--- src/main/java/com/rpietraszewski/medicalclinic/repository/VisitRepository.java
+++ src/main/java/com/rpietraszewski/medicalclinic/repository/VisitRepository.java
@@ -1,6 +1,5 @@
 package com.rpietraszewski.medicalclinic.repository;
 
-import com.rpietraszewski.medicalclinic.model.entity.Doctor;
 import com.rpietraszewski.medicalclinic.model.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
