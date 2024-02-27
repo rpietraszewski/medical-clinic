6acee7676954faf00b5acbbeb76ab913e9cfc247
Robert Pietraszewski <80597@student.pb.edu.pl>
2024-02-27 17:03:34 +0100

    + MED-97

--- src/main/java/com/rpietraszewski/medicalclinic/exception/VisitAlreadyAssignedException.java
+++ src/main/java/com/rpietraszewski/medicalclinic/exception/VisitAlreadyAssignedException.java
@@ -3,7 +3,7 @@ package com.rpietraszewski.medicalclinic.exception;
 import org.springframework.http.HttpStatus;
 
 public class VisitAlreadyAssignedException extends MedicalClinicException{
-    public VisitAlreadyAssignedException(String message) {
+    public VisitAlreadyAssignedException(String message) {                                                                                                                                  
         super(message, HttpStatus.CONFLICT);
     }
 }
