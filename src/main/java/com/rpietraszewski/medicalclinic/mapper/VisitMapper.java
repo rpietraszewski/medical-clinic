6fe48d727ca8d4e580d2398dc35f5cfd320dd14e
Robert Pietraszewski <80597@student.pb.edu.pl>
2024-02-28 19:25:03 +0100

    + MED-97

--- src/main/java/com/rpietraszewski/medicalclinic/mapper/VisitMapper.java
+++ src/main/java/com/rpietraszewski/medicalclinic/mapper/VisitMapper.java
@@ -2,46 +2,18 @@ package com.rpietraszewski.medicalclinic.mapper;
 
 import com.rpietraszewski.medicalclinic.model.dto.VisitCreateDTO;
 import com.rpietraszewski.medicalclinic.model.dto.VisitDTO;
-import com.rpietraszewski.medicalclinic.model.entity.Doctor;
-import com.rpietraszewski.medicalclinic.model.entity.Institution;
-import com.rpietraszewski.medicalclinic.model.entity.Patient;
 import com.rpietraszewski.medicalclinic.model.entity.Visit;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface VisitMapper {
-    @Mapping(source = "doctor", target = "doctorEmail", qualifiedByName = "mapToDoctorEmail")
-    @Mapping(source = "institution", target = "institution", qualifiedByName = "mapToInstitutionId")
-    @Mapping(source = "patient", target = "patientEmail", qualifiedByName = "mapToPatientEmail")
+    @Mapping(source = "doctor.email", target = "doctorEmail")
+    @Mapping(source = "institution.id", target = "institution")
+    @Mapping(source = "patient.email", target = "patientEmail")
     VisitDTO toVisitDTO(Visit visit);
 
     @Mapping(source = "institution", target = "institution", ignore = true)
     @Mapping(source = "doctorEmail", target = "doctor", ignore = true)
     Visit toVisit(VisitCreateDTO visitCreateDTO);
-
-    @Named("mapToDoctorEmail")
-    default String mapToDoctorEmail(Doctor doctor) {
-        if (doctor == null) {
-            return null;
-        }
-        return doctor.getEmail();
-    }
-
-    @Named("mapToInstitutionId")
-    default Long mapToInstitutionId(Institution institution) {
-        if (institution == null) {
-            return null;
-        }
-        return institution.getId();
-    }
-
-    @Named("mapToPatientEmail")
-    default String mapToPatientEmail(Patient patient) {
-        if (patient == null) {
-            return null;
-        }
-        return patient.getEmail();
-    }
 }
